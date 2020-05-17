8600b2d68ab2b84a1a45acb91a23531c9da4cf6f
CS-NSI <58935796+CS-NSI@users.noreply.github.com>
2020-05-17 19:34:41 +0200

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,9 @@
+$('.carousel').carousel({
+    interval: 2000
+  })
+
+$('#carouselExampleCaptions').on('slide.bs.carousel', function () {
+    // do something...
+  })$('#carouselExampleCaptions').on('slide.bs.carousel', function () {
+  // do something...
+})
